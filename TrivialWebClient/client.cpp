d32a5cfd7b40a59320718c30513a43a4d4f425cf
Ricardo0623 <42210980+Ricardo0623@users.noreply.github.com>
2019-04-28 22:49:58 -0300

    project client: main.cpp split into main.cpp, client.cpp and client.h project server: didn't split it because it already has files named server.cpp and server.h (Please check this)

---
+++ TrivialWebClient/client.cpp
@@ -0,0 +1,58 @@
+
+#include "client.h"
+
+
+
+client::client() {
+	IO_handler = new boost::asio::io_service();
+	socket_forClient = new boost::asio::ip::tcp::socket(*IO_handler);
+	client_resolver = new boost::asio::ip::tcp::resolver(*IO_handler);
+}
+
+client::~client() {
+	socket_forClient->close();
+	delete client_resolver;
+	delete socket_forClient;
+	delete IO_handler;
+}
+
+void client::startConnection(const string& host) {
+	endpoint = client_resolver->resolve(
+		boost::asio::ip::tcp::resolver::query(host, HELLO_PORT_STR));
+	cout << "Trying to connect to " << host << " on port " << HELLO_PORT_STR << std::endl;
+	boost::asio::connect(*socket_forClient, endpoint);
+	socket_forClient->non_blocking(true);
+}
+
+void client::sendMessage(const string& message) {
+
+	size_t len;
+	boost::system::error_code error;
+
+	do
+	{
+		len = socket_forClient->write_some(boost::asio::buffer(message, message.size()), error);
+	} while ((error.value() == WSAEWOULDBLOCK));
+	if (error)
+		std::cout << "Error while trying to connect to server " << error.message() << std::endl;
+}
+
+void client::receiveMessage() {
+	boost::system::error_code error;
+	char buf[512];
+	size_t len = 0;
+	cout << "Receiving Message" << std::endl;
+	do
+	{
+		len = socket_forClient->read_some(boost::asio::buffer(buf), error);
+
+		if (!error)
+			buf[len] = '\0';
+
+	} while (error.value() == WSAEWOULDBLOCK);
+
+	if (!error)
+		std::cout << std::endl << "Server says: " << buf << std::endl;
+	else
+		std::cout << "Error while trying to connect to server " << error.message() << std::endl;
+}
